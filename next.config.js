ddd7abc257b2cb108591bfcd35c502f5b6bc70cc
accoladesio <ononobip@gmail.com>
2024-10-22 14:24:11 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  }
+});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/Wave.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/Wave.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
