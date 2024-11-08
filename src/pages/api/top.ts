d7bff3de446e46be8f0393c086dc40993fab4847
accoladesio <ononobip@gmail.com>
2024-11-08 01:24:33 +0100

    feat: fetched top tracks, top artists and top genre

--- src/pages/api/top.ts
+++ src/pages/api/top.ts
@@ -1,11 +1,41 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+interface SpotifyImage {
+    url: string;
+    height: number;
+    width: number;
+}
 
-async function fetchFromSpotify(endpoint: string, token: string) {
+interface Track {
+    id: string;
+    name: string;
+    artists: Artist[];
+    album: Album;
+}
+
+interface Artist {
+    id: string;
+    name: string;
+    genres: string[];
+    images: SpotifyImage[];
+}
+
+interface Album {
+    id: string;
+    name: string;
+    images: SpotifyImage[];
+}
+
+interface SpotifyApiResponse<T> {
+    items: T[];
+}
+
+
+async function fetchFromSpotify<T>(endpoint: string, token: string): Promise<SpotifyApiResponse<T>> {
     const response = await fetch(`https://api.spotify.com/v1/${endpoint}`, {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
+            Authorization: `Bearer ${token}`,
+        },
     });
     if (!response.ok) {
         throw new Error(`Spotify API error: ${response.statusText}`);
@@ -21,40 +51,36 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
 
     try {
-        const accessToken = session.accessToken;
+        const accessToken = session.accessToken as string;
 
         // Get top tracks
-        const topTracks = await fetchFromSpotify('me/top/tracks?time_range=short_term&limit=20', accessToken);
+        const topTracks = await fetchFromSpotify<Track>('me/top/tracks?time_range=short_term&limit=20', accessToken);
 
         // Get top artists
-        const topArtists = await fetchFromSpotify('me/top/artists?time_range=short_term&limit=20', accessToken);
+        const topArtists = await fetchFromSpotify<Artist>('me/top/artists?time_range=short_term&limit=20', accessToken);
 
         // Extract top albums from top tracks
-        const topAlbums = topTracks.items.map((track: any) => track.album).reduce((uniqueAlbums: any[], album: any) => {
-            if (!uniqueAlbums.some((a) => a.id === album.id)) uniqueAlbums.push(album);
-            return uniqueAlbums;
-        }, []);
+        const topAlbums = topTracks.items
+            .map((track) => track.album)
+            .reduce<Album[]>((uniqueAlbums, album) => {
+                if (!uniqueAlbums.some((a) => a.id === album.id)) uniqueAlbums.push(album);
+                return uniqueAlbums;
+            }, []);
 
         // Extract top genres from top artists
-        const topGenres = Array.from(
-            new Set(
-                topArtists.items.flatMap((artist: any) => artist.genres)
-            )
-        );
+        const topGenres = Array.from(new Set(topArtists.items.flatMap((artist) => artist.genres)));
 
         res.status(200).json({
             topTracks: topTracks.items,
             topArtists: topArtists.items,
             topAlbums,
-            topGenres
+            topGenres,
         });
     } catch (error: unknown) {
         if (error instanceof Error) {
-            // Now you can safely access `error.message`
             res.status(500).json({ error: error.message });
         } else {
-            // If error is not an instance of `Error`
-            res.status(500).json({ error: "An unknown error occurred" });
+            res.status(500).json({ error: 'An unknown error occurred' });
         }
     }
 }
