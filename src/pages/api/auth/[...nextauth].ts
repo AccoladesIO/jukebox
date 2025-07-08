b8edf25139397f3735bb3e09414a88a2ff34031e
Ononobi Praise <ononobip@gmail.com>
2025-07-08 22:58:34 +0100

    new fix

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -105,11 +105,6 @@ export const authOptions: AuthOptions = {
             };
         },
     },
-    pages: {
-        signIn: "/auth/signin",
-        signOut: "/auth/signout",
-        error: "/auth/error",
-    },
     session: {
         strategy: "jwt",
     },
